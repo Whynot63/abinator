110eed85a036182838fca8d84528908cd39ea432
Dmitry Andreev <da@shiftam.com>
2023-03-09 15:52:38 +0100

    added help function

--- abinator/__init__.py
+++ abinator/__init__.py
@@ -3,3 +3,7 @@ from .events import Event, anonymous, indexed
 from .inputs import Struct
 from .state_mutability import payable, view, pure
 from .types import *
+
+
+def to_abi(abi_cls: Abi) -> list:
+    return abi_cls.to_abi()
